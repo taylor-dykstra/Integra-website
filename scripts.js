7f6792d0b7025b811f8d1bb32055c96158c4fe08
taylor-dykstra <118199667+taylor-dykstra@users.noreply.github.com>
2025-05-20 15:01:48 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carousel-item');
+
 function showSlide(index) {
   slides.forEach((slide, i) => {
     slide.classList.toggle('active', i === index);
   });
 }
+
 function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   showSlide(currentSlide);
 }
+
 setInterval(nextSlide, 4000);
 showSlide(currentSlide);
 
